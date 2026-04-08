894913eabbc88035badb84f3183712c3005e409c
Asus\User <hengyanchan1@gmail.com>
2026-04-08 21:33:31 +0800

    update

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -42,7 +42,7 @@ class Invoice extends Model
     ];
     
     protected $casts = [
-        'date' => 'datetime',
+        'date' => 'date',
         'is_driver' => 'boolean',
         'created_by' => 'integer',
     ];
