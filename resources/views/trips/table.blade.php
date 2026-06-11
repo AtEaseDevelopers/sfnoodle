34ab6f8e54834fe6939f6a42d71c677bc4b3cda1
Asus\User <hengyanchan1@gmail.com>
2026-06-11 23:06:38 +0800

    update

--- resources/views/trips/table.blade.php
+++ resources/views/trips/table.blade.php
@@ -63,7 +63,7 @@
                 noti('i','Date To cannot be empty','Please select the Date To');
                 return;
             }
-            if($('#datetomodel').val() < $('#datefrommodel').val()){
+            if(moment($('#datetomodel').val(), 'DD-MM-YYYY').isBefore(moment($('#datefrommodel').val(), 'DD-MM-YYYY'))){
                 noti('i','Date From cannot greater than Date To','Please select the Date again');
                 return;
             }
